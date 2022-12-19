5e633e6b4ff5a6dc530e9524ee46e3dc6dd7f777
jackji <jichengde@gmail.com>
2022-12-19 17:14:54 +0800

    day18

--- day18.go
+++ day18.go
@@ -14,10 +14,11 @@ type Cube struct {
 var (
 	cubes            []Cube
 	space            [][][]bool
+	airs             [][][]bool
 	xLen, yLen, zLen int
 )
 
-func getSurface(cube Cube) int {
+func getSurface1(cube Cube) int {
 	var s = 0
 	if cube.x == 0 || !space[cube.z][cube.y][cube.x-1] {
 		s++
@@ -35,6 +36,29 @@ func getSurface(cube Cube) int {
 		s++
 	}
 	if cube.z == zLen-1 || !space[cube.z+1][cube.y][cube.x] {
+		s++
+	}
+	return s
+}
+
+func getSurface2(cube Cube) int {
+	var s = 0
+	if cube.x == 0 || airs[cube.z][cube.y][cube.x-1] {
+		s++
+	}
+	if cube.x == xLen-1 || airs[cube.z][cube.y][cube.x+1] {
+		s++
+	}
+	if cube.y == 0 || airs[cube.z][cube.y-1][cube.x] {
+		s++
+	}
+	if cube.y == yLen-1 || airs[cube.z][cube.y+1][cube.x] {
+		s++
+	}
+	if cube.z == 0 || airs[cube.z-1][cube.y][cube.x] {
+		s++
+	}
+	if cube.z == zLen-1 || airs[cube.z+1][cube.y][cube.x] {
 		s++
 	}
 	return s
@@ -62,21 +86,74 @@ func main() {
 		cubes = append(cubes, Cube{x: x, y: y, z: z})
 	}
 
-	// Init space
 	space = make([][][]bool, zLen)
+	airs = make([][][]bool, zLen)
 	for i := 0; i < zLen; i++ {
 		space[i] = make([][]bool, yLen)
+		airs[i] = make([][]bool, yLen)
 		for j := 0; j < yLen; j++ {
 			space[i][j] = make([]bool, xLen)
+			airs[i][j] = make([]bool, xLen)
 		}
 	}
 	for _, c := range cubes {
 		space[c.z][c.y][c.x] = true
 	}
 
+	//part 1
 	var surface = 0
 	for _, c := range cubes {
-		surface += getSurface(c)
+		surface += getSurface1(c)
+	}
+	fmt.Println(surface)
+
+	//part 2
+	// expand air, from outside to inside
+	var airN = 0
+	for z := 0; z < zLen; z++ {
+		for y := 0; y < yLen; y++ {
+			for x := 0; x < xLen; x++ {
+				if !(z == 0 || z == zLen-1 ||
+					y == 0 || y == yLen-1 ||
+					x == 0 || x == xLen-1) {
+					continue
+				}
+				airs[z][y][x] = !space[z][y][x]
+				if airs[z][y][x] {
+					airN++
+				}
+			}
+		}
+	}
+	for {
+		var newAirN = 0
+		for z := 0; z < zLen; z++ {
+			for y := 0; y < yLen; y++ {
+				for x := 0; x < xLen; x++ {
+					if airs[z][y][x] || space[z][y][x] {
+						continue
+					}
+					if airs[z][y][x-1] ||
+						airs[z][y][x+1] ||
+						airs[z][y-1][x] ||
+						airs[z][y+1][x] ||
+						airs[z-1][y][x] ||
+						airs[z+1][y][x] {
+						airs[z][y][x] = true
+						newAirN++
+						continue
+					}
+				}
+			}
+		}
+		if newAirN == 0 {
+			break
+		}
+		airN += newAirN
+	}
+	surface = 0
+	for _, c := range cubes {
+		surface += getSurface2(c)
 	}
 	fmt.Println(surface)
 }
